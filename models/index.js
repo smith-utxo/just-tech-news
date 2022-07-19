502f9df27b2566cf3b11a74ad709c5fcd479e18f
Adam Smith <smith.adam937@gmail.com>
2022-07-19 14:16:46 -0500

    post model created to associate user with post

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 //Purpose of index.js is mainly for collecting and exporting the user model data
 
 const User = require('./User'); 
+const Post = require('./Post');
 
-module.exports = { User };
+module.exports = { User, Post };
